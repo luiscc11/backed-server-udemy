d2364a5431efcde9ec80f8b151f20b528d09ee8d
Luis <lcasillas@anzen.com.mx>
2020-02-07 11:14:39 -0600

    Usuario y token listo

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 // Requires
 var express = require('express');
 var mongoose = require('mongoose');
-
+var bodyParser = require('body-parser');
 
 // Inizializar variables
 var app = express();
+
+
+// body parser
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Importar rutas
+var appRoutes = require('./routes/app');
+var usuarioRoutes = require('./routes/usuario');
+var loginRoutes = require('./routes/login');
 
 
 // Conexion a BD
@@ -16,12 +27,9 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (err, res) =
 
 
 // Rutas
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        ok: true,
-        mensaje: 'Peticion realizada correctamente'
-    });
-});
+app.use('/usuario', usuarioRoutes);
+app.use('/login', loginRoutes);
+app.use('/', appRoutes);
 
 
 // Escuchar peticiones
---
+++ config/config.js
@@ -0,0 +1,6 @@
+
+
+
+
+
+module.exports.SEED = '@este-es@-un-seed-dificil';
